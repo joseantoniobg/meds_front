4e1d6ea075cb8600e2bec474cf58034de8e8955c
José Antônio <joseantonio1@hotmail.com.br>
2025-05-07 08:04:09 -0300

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Provider } from "@/components/ui/provider"
 import { AuthProvider } from "@/contexts/auth.context"
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
@@ -13,6 +14,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
       </head>
       <body>
         <AuthProvider>
+          <Analytics />
           <Provider><><Toaster />{children}</></Provider>
         </AuthProvider>
       </body>
